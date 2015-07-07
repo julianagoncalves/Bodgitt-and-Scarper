d00705cd1b3373816e606303fd051f4873cc45da
Juliana Goncalves <julianagoncalves.jgp@gmail.com>
2015-07-07 01:06:06 -0300

    Incrementando método para leitura do cabeçalho e dados do arquivo

--- data_connection.py
+++ data_connection.py
@@ -18,24 +18,35 @@ class DataConnection:
         self.data_file.seek(0,2)
         self.data_file.seek(0) 
 
-        self.magic_cookie = self.reading_fields_binary(4)
-        self.total_each_record = self.reading_fields_binary(4)
+        magic_cookie = self.reading_fields_binary(4)
+        total_each_record = self.reading_fields_binary(4)
         self.number_fields = self.reading_fields_binary(2)
+        
+        dict_fields = self.reading_fields_head()
+        dict_fields_data = {}
 
-        self.reading_fields()
+        for i in range(0,self.number_fields[1]):
+            field_and_byte = dict_fields.items()[i]
+            field = field_and_byte[0]
+            byte = field_and_byte[1]
+
+            data = self.reading_fields_string(byte)
+
+            dict_fields_data[field] = data
+
         
-    def reading_fields(self):
-        dict_file = {}
-        
+    def reading_fields_head(self):
+        dict_fields = {}
+
         for i in range(0,self.number_fields[1]):
 
             size_name_of_field = self.reading_fields_binary(2)
             name_of_field = self.reading_fields_string(size_name_of_field[-1])
             size_of_field_bytes = self.reading_fields_binary(2)
 
-            dict_file[name_of_field[0]] = size_of_field_bytes[1]
+            dict_fields[name_of_field[0]] = size_of_field_bytes[1]
 
-        return dict_file
+        return dict_fields
 
     def reading_fields_binary(self, number_of_bytes):
 
